acab704581065f8bd6ea6874fa4cc8f1d9337942
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2021-02-15 14:31:29 -0800

    Refine data, columns, and calculations
    - Extract date, tournament, player names, and other columns from
      match_id
    - Add row for final score
    - Draw player names on chart
    - Filter training set to only include 2008-2020
    - Fix string encoding issues

--- tennis-win-probability.R
+++ tennis-win-probability.R
@@ -59,32 +59,46 @@ retrieve_csv_and_cache_data_locally <- function(url) {
   return(data)
 }
 
-# Fields
-# PtWinner : Index of player who won the point (last of match is match winner)
-# Svr : Index of player who is serving
-
-
-
-# TODO: Write for tennis
+# Plot a single match.
+#
+# Returns the plot which can be saved to disk or displayed otherwise.
 plot_for_data <-
   function(data,
            foreground_color,
            background_color) {
-    single_match_id <- data[1,]$match_id
+    this_match <- data[1,]
 
     plot <- ggplot(data,
                    aes(x = Pt, y = win_probability_player_1)) +
       # 50% reference line
       geom_hline(yintercept = 0.5,
-                 color = grey,
+                 color = foreground_color,
                  size = 1) +
-
 
       # Win Probability
       geom_line(size = 0.8) +
 
+      annotate(
+        "text",
+        x = 5,
+        y = 0.95,
+        label = this_match$player1,
+        family = "InputMono",
+        color = foreground_color,
+        size = 2
+      ) +
+
+      annotate(
+        "text",
+        x = 5,
+        y = 0.05,
+        label = this_match$player2,
+        family = "InputMono",
+        color = foreground_color,
+        size = 2
+      ) +
+
       # Formatting
-      # scale_x_reverse() +
       scale_y_continuous(labels = percent, limits = c(0, 1)) +
       theme_high_contrast(
         base_family = "InputMono",
@@ -94,11 +108,13 @@ plot_for_data <-
       theme(axis.text.x = element_blank(),
             axis.ticks.x = element_blank()) +
       labs(
-        title = str_interp("${single_match_id}"),
-        subtitle = "Custom win probability model for tennis",
+        title = str_interp(
+          "${this_match$player1} vs ${this_match$player2} @ ${this_match$tournament} ${this_match$date}"
+        ),
+        subtitle = "Custom win probability model for women's tennis",
         caption = "Data from https://github.com/JeffSackmann/tennis_MatchChartingProject",
         x = "Plays",
-        y = "Player 1 Win Probability"
+        y = "Win Probability"
       )
   }
 
@@ -111,7 +127,6 @@ build_win_prediction_model <- function(data) {
                    replace = FALSE)
   train <- data[indexes, ]
 
-  # Fields:
   win_prediction_model = glm(match_winner_is_player_1 ~
                                Set1 + Set2 + Gm1 + Gm2,
                              train,
@@ -130,41 +145,113 @@ populate_each_row_with_prediction <- function(pbp) {
 }
 
 
-plot_for_match_id <- function(single_match_id) {
-  plot <- plot_for_data(pbp %>% filter(match_id == single_match_id),
-                        "black",
-                        "white")
+plot_for_match_id <- function(data, single_match_id) {
+  single_match_records <- data %>% filter(match_id == single_match_id)
+
+  plot <- plot_for_data(single_match_records,
+                        "white",
+                        dayglo_orange)
   ggsave(
     str_interp("out/${single_match_id}.png"),
     plot = plot,
-    width = 6,
+    width = 8,
     height = 4
   )
-
 }
 
-run <- function() {
-  pbp <- charting_w_points()
+load_and_clean_data <- function() {
+  pbp <- charting_w_points() %>%
+    # Fix problematic encoding on some rows
+    mutate(match_id = iconv(match_id, "ASCII", "UTF-8")) %>%
+    # The `separate` function splits a string like "20200823-A-B-C" on dashes.
+    # Grab only the first piece as a new `date_string` column.
+    separate(
+      match_id,
+      c(
+        "date_string",
+        "gender",
+        "tournament",
+        "round",
+        "player1",
+        "player2"
+      ),
+      remove = FALSE,
+      sep = "-"
+    ) %>%
+    mutate(
+      date = as.Date(date_string, format = "%Y%m%d"),
+      player1 = str_replace(player1, "_", " "),
+      player2 = str_replace(player2, "_", " ")
+    )
 
+  # Get final play to determine match winner
   final_play_for_each_match <-
     pbp %>%
     group_by(match_id) %>%
     filter(Pt == max(Pt)) %>%
-    mutate(match_winner_is_player_1 = ifelse(PtWinner == 1, 1, 0)) %>%
+    mutate(match_winner_is_player_1 = ifelse(PtWinner == 1, 1, 0))
+
+  # Select only a few fields
+  match_winners <- final_play_for_each_match %>%
     select(match_id, match_winner_is_player_1)
 
+  # Join so all rows include the match winner
   pbp <-
     pbp %>%
-    inner_join(final_play_for_each_match, by = "match_id")
+    inner_join(match_winners, by = "match_id")
 
-  pbp <- populate_each_row_with_prediction(pbp)
+  # Create rows for final outcome.
+  # The point by point frames don't include the final score.
+  match_result_plays <- final_play_for_each_match %>%
+    mutate(
+      Pt = Pt + 1,
+      Set1 = ifelse(match_winner_is_player_1 == 1, Set1 + 1, Set1),
+      Set2 = ifelse(match_winner_is_player_1 == 0, Set2 + 1, Set2),
+      Gm1 = 0,
+      Gm2 = 0,
+      Pts = "0-0"
+    ) %>%
+    select(
+      match_id,
+      Pt,
+      Set1,
+      Set2,
+      Gm1,
+      Gm2,
+      Pts,
+      match_winner_is_player_1,
+      date,
+      gender,
+      tournament,
+      round,
+      player1,
+      player2
+    )
 
-  plot_for_match_id("20190928-W-Wuhan-F-Aryna_Sabalenka-Alison_Riske")
+  pbp <- bind_rows(pbp, match_result_plays)
+  return(pbp)
+}
+
+run <- function() {
+  match_ids <- list(
+    "20190928-W-Wuhan-F-Aryna_Sabalenka-Alison_Riske",
+    "20190325-W-Miami-R16-Caroline_Wozniacki-Su_Wei_Hsieh",
+    "20190325-W-Miami-R16-Ashleigh_Barty-Kiki_Bertens",
+    "20080705-W-Wimbledon-F-Venus_Williams-Serena_Williams"
+  )
+
+  pbp <- load_and_clean_data() %>%
+    filter(date > as.Date("2008-01-01")) %>%
+    populate_each_row_with_prediction()
+
+  for (single_match_id in match_ids) {
+    plot_for_match_id(pbp, single_match_id)
+  }
 
   # For debugging...create a frame with just this match
   single_match_pbp <-
     pbp %>%
-    filter(match_id == "20190928-W-Wuhan-F-Aryna_Sabalenka-Alison_Riske") %>%
+    filter(match_id == single_match_id) %>%
     select(
       match_id,
       Pt,
